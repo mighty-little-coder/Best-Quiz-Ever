192e2c88c2fd6fbcb779a1a7e4c8f0526061ade2
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-10-12 00:48:41 -0400

    Added CSS and JS to make switching between DISPLAY ANSWERS and LOG YOUR SCORE functional

--- assets/quiz_script.js
+++ assets/quiz_script.js
@@ -40,47 +40,64 @@ var questions = [
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "Who is responsible for the overall direction of the project on set?",
+    answersArray: ["Key grip", "Director of Photography (DP)", "Audio Engineer", "Camera operator"],
+    correctAnswer: "Director of Photography (DP)"
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "Which size resolution is commonly known as \"4K\"?",
+    answersArray: ["720px x 480px", "1920px x 1080px", "3840px x 2160px", "7680px x 4320px"],
+    correctAnswer: "3840px x 2160px"
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "Which lens would show the shallowest depth of field at the same focal length?",
+    answersArray: ["8.0", "5.6", "2.0", "1.4"],
+    correctAnswer: "1.4"
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "What is focal length?",
+    answersArray: ["Distance from a lens to it's point of focus", "Distance a lens has the ability to keep in focus", "Length of the len's glass elements", "The length of time a lense can remain focused on one element"],
+    correctAnswer: "Distance from a lens to it's point of focus"
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "What is directly effected by the \"F-stop\" of a lens",
+    answersArray: ["shutter speed", "ISO", "frames per second", "aperture size"],
+    correctAnswer: "aperture size"
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "Which ISO would allow for the most amount of added light to an image / shot?",
+    answersArray: ["200", "400", "1000", "3200"],
+    correctAnswer: "3200"
   },
 
   {
-    questionTitle: "Which brand has the largest array of lens available?",
-    answersArray: ["Sony", "Lumix", "Canon", "Fujifilm"],
-    correctAnswer: "Canon"
+    questionTitle: "What does the \"speed\" of a lens refer to?",
+    answersArray: ["How fast a lens captures images", "How slow a camera can record to capture smooth slow motion footage", "Maximum aperture diameter", "Minimum aperture diameter"],
+    correctAnswer: "Maximum aperture diameter"
   },
 
+  {
+    questionTitle: "Where does the term \"film grain\" come form?",
+    answersArray: ["Small bits of film that is fed to birds", "Granularity of a photo or video from when film was the predominant media", "Film gluten", "Instagram filter"],
+    correctAnswer: "Granularity of a photo or video from when film was the predominant media"
+  },
+
+  {
+    questionTitle: "What is a \"LUT\"?",
+    answersArray: ["Camera brand", "IDK. Google it", "Colorizing preset", "Term used for a someone working on set"],
+    correctAnswer: "Colorizing preset"
+  },
+
+  {
+    questionTitle: "Which frame rate would produce the clearest slow motion footage?",
+    answersArray: ["24fps", "60fps", "120fps", "240fps"],
+    correctAnswer: "240fps"
+  },
 ]
 
 document.querySelector(".startButton").addEventListener("click", setTime)
